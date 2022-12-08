83c0add2ee0d70a0ecb517434d1c0b7425d8e5a8
Nicholas Ferraz <9021595+nikoferraz@users.noreply.github.com>
2022-12-07 21:10:24 -0600

    Day 7

--- 2022/7/make_room.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-input = []
-with open("input.txt", "r") as file:
-    for line in file.readlines():
-        input.append(line.strip("\n").split())
-
-print(input)
